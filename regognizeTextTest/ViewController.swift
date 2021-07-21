11fecbd7633121ebd00513528e26c544821fa797
minminmikan <ff.remon.9123@gmail.com>
2021-07-21 15:13:16 +0900

    Initial Commit

---
+++ regognizeTextTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  regognizeTextTest
+//
+//  Created by minmin on 2021/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
